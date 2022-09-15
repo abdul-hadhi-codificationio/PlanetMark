04849ced59f4d60feddfdae7b38a2ef3eb0d405c
multipacman <abdulhadhee1@gmail.com>
2022-09-16 04:28:32 +0530

    refactored files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import SidebarWithHeader from './Navigation/sidebar';
-import { Dashboard } from './Pages/dashboard';
+import SidebarWithHeader from './Navigation/Sidebar';
+import { Dashboard } from './Pages/Dashboard/DashboardPage';
 import { Company } from './Pages/Company/CompanyPage';
 
 function App() {
